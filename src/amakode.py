1875b2a6f760751ed40fa28dddb541c60ba7e403
htrd <htrd@f7610ada-f980-4811-9640-8f58bd4d241a>
2008-09-07 07:46:20 +0000

    added project files. updated copyright.

--- src/amakode.py
+++ src/amakode.py
@@ -23,6 +23,7 @@
 #
 # Copyright (C) 2007 Daniel O'Connor. All rights reserved.
 # Copyright (C) 2007 Jens Zurheide. All rights reserved.
+# Copyright (C) 2008 Toby Dickenson. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
